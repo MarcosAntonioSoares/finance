ed7909fb72a7709112a6c058d31963bfdbe4a05a
Marcos <marcosas.soares2@gmail.com>
2024-06-10 22:53:16 -0300

    Update dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
-import dotenv from "dotenv";
 
-dotenv.config({
-  path: ".env.local",
-});
+import { config } from "dotenv";
 
 import { defineConfig } from "drizzle-kit";
+
+config({path: ".env.local"})
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
